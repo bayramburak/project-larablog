9f3e1b9941e25a7dc4f4ff2d047bedf334d64418
Burak Bayram <karubmaryab@gmail.com>
2021-02-07 21:29:36 +0300

    navbar hamburger menu bugfix

--- resources/views/front/layouts/inc/head.blade.php
+++ resources/views/front/layouts/inc/head.blade.php
@@ -1,6 +1,12 @@
 <!-- Styles -->
 <!-- Bootstrap CSS -->
 <link href="{{asset('css/bootstrap.min.css')}}" rel="stylesheet">
+
+
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" ></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js" ></script>
+
+
 <!-- Font awesome CSS -->
 <link href="{{asset('css/font-awesome.min.css')}}" rel="stylesheet">
 <!-- Custom CSS -->
--- resources/views/front/layouts/inc/header.blade.php
+++ resources/views/front/layouts/inc/header.blade.php
@@ -4,17 +4,20 @@
         <div class="container">
             <!-- Brand and toggle get grouped for better mobile display -->
             <div class="navbar-header">
-
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navbarNavAltMarkup">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
                 <a class="navbar-brand" href="{{route('homepage')}}"><img class="img-responsive" src="img/logo.png"
                                                                           alt="Logo"/></a>
             </div>
 
             <!-- Collect the nav links, forms, and other content for toggling -->
-            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+            <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
                 <ul class="nav navbar-nav navbar-right">
-
-
-                    <li><a href="{{route('blog')}}">Blog</a></li>
+                    <li class="nav-item"><a href="{{route('blog')}}">Blog</a></li>
                     <li><a href="{{route('events')}}">Events</a></li>
                     <li><a href="{{route('team')}}">Team</a></li>
                     <li><a href="{{route('subscribe')}}">Subscribe</a></li>
